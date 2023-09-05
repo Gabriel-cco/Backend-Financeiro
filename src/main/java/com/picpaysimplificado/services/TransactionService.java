26e4ed7f416051c077c5582b0b31fd123f9c6bcd
gabriel <g.batista@kroton.com.br>
2023-09-05 11:20:35 -0300

    Refactor: change in methods

--- src/main/java/com/picpaysimplificado/services/TransactionService.java
+++ src/main/java/com/picpaysimplificado/services/TransactionService.java
@@ -4,7 +4,9 @@ import com.picpaysimplificado.domain.transaction.Transaction;
 import com.picpaysimplificado.domain.user.User;
 import com.picpaysimplificado.dtos.TransactionDTO;
 import com.picpaysimplificado.repositories.TransacionRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -26,6 +28,11 @@ public class TransactionService {
     @Autowired
     private NotificationService notificationService;
 
+    private static final String URL_AUTHORIZATION = "https://run.mocky.io/v3/8fafdd68-a090-496f-8c9a-3442cf30dae6";
+
+    @Value("${notification.sucesso}")
+    private static String sucesso;
+
     public Transaction createTransaction(TransactionDTO transaction){
         User send = userService.findById(transaction.sender());
         User receiver = userService.findById(transaction.receiver());
@@ -34,25 +41,21 @@ public class TransactionService {
         if(!isAuthorized){
             throw new RuntimeException("Transação não autorizada");
         }
-        Transaction transactionSave = Transaction.builder()
-                .amount(transaction.amount())
-                .sender(send)
-                .receiver(receiver)
-                .build();
-
+        Transaction transactionSave = new Transaction();
+        BeanUtils.copyProperties(transaction, transactionSave);
         send.setBalance(send.getBalance().subtract(transaction.amount()));
         receiver.setBalance(receiver.getBalance().add(transaction.amount()));
         repository.save(transactionSave);
         userService.save(send);
         userService.save(receiver);
-/*        notificationService.sendNotification(send, "Transação relizada com sucesso");
-        notificationService.sendNotification(receiver, "Transação relizada com sucesso");*/
+/*        notificationService.sendNotification(send, sucesso);
+        notificationService.sendNotification(receiver, sucesso);*/
         return transactionSave;
 
     }
 
     private boolean authorizationTransaction(User sender, BigDecimal value){
-      ResponseEntity<Map> responseAuthorization = restTemplate.getForEntity("https://run.mocky.io/v3/8fafdd68-a090-496f-8c9a-3442cf30dae6", Map.class);
+      ResponseEntity<Map> responseAuthorization = restTemplate.getForEntity(URL_AUTHORIZATION, Map.class);
       if(responseAuthorization.getStatusCode() == HttpStatus.OK){
           return "Autorizado".equals(responseAuthorization.getBody().get("message"));
       }else return false;
